4875ef4bfb23c20816a8f7b8a4dae4d6ed1abe6b
Jean Carlo Hilger <hilgerjeancarlo@gmail.com>
2022-06-15 02:17:50 -0300

    feat: Adds local search 2-opt

---
+++ solutions/local_search/blpm2opt.py
@@ -0,0 +1,55 @@
+import time
+import numpy as np
+from math import inf
+from typing import Union
+from ..solution import Solution
+
+class BLPM2opt(Solution):
+    def __init__(self):
+        super(BLPM2opt, self).__init__()
+    
+    def solve(self, adjacency_matrix: np.ndarray, **kwargs: dict) -> np.ndarray:
+        assert "timelimit" in kwargs
+
+        timelimit = kwargs.pop("timelimit")
+
+        initial_path = np.arange(len(adjacency_matrix))
+
+        np.random.shuffle(initial_path)
+
+        return self._solve(
+                initial_path, adjacency_matrix,
+                time.time(), timelimit)
+    
+    def _solve(
+            self, initial_path: np.ndarray,
+            adjacency_matrix: np.ndarray,
+            start_time:int, timelimit: int) -> np.ndarray:
+        current_path = initial_path.copy()
+        current_cost = self.evaluate(adjacency_matrix, initial_path)
+
+        generated_path = initial_path.copy()
+        generated_cost = None
+
+        for i in range(1, len(adjacency_matrix) - 1):
+            for j in range(i + 1, len(adjacency_matrix)):
+                
+                if time.time() - start_time > timelimit:
+                    break
+
+                generated_path = np.concatenate([
+                    current_path[:i],
+                    np.flip(current_path[i:j + 1]),
+                    current_path[j + 1:],
+                ])
+
+                generated_cost = self.evaluate(
+                            adjacency_matrix, generated_path)
+
+                if generated_cost < current_cost:
+                    return self._solve(
+                            generated_path, adjacency_matrix,
+                            start_time, timelimit)
+        
+        return generated_path
+    
